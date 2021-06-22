c0270a37b580f44bae10ad697caceb92e8025c7c
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-06-22 16:23:56 +0530

    Add comments for all apps

--- home.nix
+++ home.nix
@@ -6,72 +6,72 @@
   home.homeDirectory = "/home/khushraj";
 
   imports = [
-    ./chromium.nix
-    ./fish.nix
-    ./git.nix
-    ./i3status-rust.nix
-    ./kitty.nix
-    ./rofi.nix
-    ./starship.nix
+    ./chromium.nix            # Web browser
+    ./fish.nix                # Shell
+    ./git.nix                 # Version control
+    ./i3status-rust.nix       # Statusbar information
+    ./kitty.nix               # Terminal emulator
+    ./rofi.nix                # Application launcher
+    ./starship.nix            # Shell prompt
   ];
 
   home.packages = [
 #    pkgs.authy
-    pkgs.etcher
-    pkgs.bitwarden
-    pkgs.bottom
-    pkgs.calibre
-    pkgs.discord
-    pkgs.dunst
-    pkgs.du-dust
-    pkgs.efibootmgr
-    pkgs.electrum
-    pkgs.fd
-    pkgs.flameshot
-    pkgs.gitkraken
-    pkgs.google-chrome
-    pkgs.grex
-    pkgs.handbrake
-    pkgs.hyperfine
-    pkgs.imagemagick
-    pkgs.insomnia
-    pkgs.jdk
-    pkgs.libreoffice
-    pkgs.lm_sensors
-    pkgs.lxappearance
-    pkgs.neofetch
-    pkgs.notion
-    pkgs.p7zip
-    pkgs.procs
-    pkgs.qbittorrent
-    pkgs.ripgrep
-    pkgs.rustup
-    pkgs.sd
-    pkgs.signal-desktop
-    pkgs.simplescreenrecorder
-    pkgs.slack
-    pkgs.speedtest-cli
-    pkgs.spotify
-    pkgs.tealdeer
-    pkgs.teams
-    pkgs.teamviewer
-    pkgs.tdesktop
-    pkgs.virtualbox
+    pkgs.etcher               # Disk image flasher
+    pkgs.bitwarden            # Password manager
+    pkgs.bottom               # Terminal-based task viewer
+    pkgs.calibre              # Ebook library
+    pkgs.discord              # Communications app
+    pkgs.dunst                # Notifications daemon
+    pkgs.du-dust              # Terminal-based storage space viewer
+    pkgs.efibootmgr           # EFI boot entry manager
+    pkgs.electrum             # Bitcoin wallet
+    pkgs.fd                   # File finder
+    pkgs.flameshot            # Screenshot tool
+    pkgs.gitkraken            # Git GUI client
+    pkgs.google-chrome        # Browser
+    pkgs.grex                 # Regex maker
+    pkgs.handbrake            # Audio-video encoder
+    pkgs.hyperfine            # Benchmarking tool
+    pkgs.imagemagick          # Terminal-based photo modifier
+    pkgs.insomnia             # REST client
+    pkgs.jdk                  # Java development kit 
+    pkgs.libreoffice          # Office suite
+    pkgs.lm_sensors           # Hardware sensors. Required for i3status-rust
+    pkgs.lxappearance         # GTK theme editor
+    pkgs.neofetch             # Terminal-based system-info viewer
+    pkgs.notion               # Document manager / Collaboration
+    pkgs.p7zip                # Compression tool
+    pkgs.procs                # Terminal job viewer
+    pkgs.qbittorrent          # Torrent client
+    pkgs.ripgrep              # File-content searcher
+    pkgs.rustup               # Rust toolchain manager
+    pkgs.sd                   # stdin filterer
+    pkgs.signal-desktop       # Communications app
+    pkgs.simplescreenrecorder # Screen recorder
+    pkgs.slack                # Communications app
+    pkgs.speedtest-cli        # Network speed measurer
+    pkgs.spotify              # Music app
+    pkgs.tealdeer             # Manual summarizer
+    pkgs.teams                # Video call app
+    pkgs.teamviewer           # Remote desktop app
+    pkgs.tdesktop             # Communications app
+    pkgs.virtualbox           # Hardware virtualizer
 #    pkgs.virtualboxExtpack
-    pkgs.viu
-    pkgs.vlc
-    pkgs.youtube-dl
-    pkgs.zoom
+    pkgs.viu                  # Terminal image viewer
+    pkgs.vlc                  # Audio/video player
+    pkgs.youtube-dl           # Multimedia scraper
+    pkgs.zoom                 # Video call app
   ];
 
   # Temp
-  programs.bat.enable = true;
-  programs.exa.enable = true;
-  programs.firefox.enable = true;
+  programs.bat.enable = true; # File contents viewer
+  programs.exa.enable = true; # File lister
+  programs.firefox.enable = true; # Web browser
   programs.firefox.package = pkgs.firefox-devedition-bin;
-  programs.gh.enable = true;
-  programs.obs-studio.enable = true;
-  programs.vscode.enable = true;
+  programs.gh.enable = true; # GitHub client
+  programs.obs-studio.enable = true; # Advanced screen recorder
+  programs.vscode.enable = true; # Code editor
   # programs.cheese.enable = true;
   
   home.stateVersion = "21.05";
