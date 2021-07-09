e0a17750d6efa441445145e7a5f591fa090d253d
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-07-09 13:36:30 +0530

    Add rclone sync timer and service

--- nixpkgs/services/rclone.nix
+++ nixpkgs/services/rclone.nix
@@ -1,23 +1,36 @@
 { pkgs, ... }: 
 
 {
-	systemd.user.services = {
-		rclone-mount = {
-			Unit = {
-				Description = "Backup and sync daemon [MOUNT]";
+	systemd.user = {
+		services = {
+			rclone-mount = {
+				Unit.Description = "Backup and sync daemon [MOUNT]";
+				Install.WantedBy = [ "multi-user.target" ];
+				Service = {
+					Type = "simple";
+					ExecStart = "${pkgs.rclone}/bin/rclone mount google-drive: /home/khushraj/google-drive/ --vfs-cache-mode full";
+					Environment = "PATH=/run/wrappers/bin/:$PATH";
+					Restart = "always";
+					RestartSec = 1;
+				};
 			};
-			Install = {
-				Wants = [ "multi-user.target" ];
-				WantedBy = [ "multi-user.target" ];
-				After = [ "multi-user.target" ];
+			rclone-sync = {
+				Install.WantedBy = [ "multi-user.target" ];
+				Service.ExecStart = "${pkgs.rclone}/bin/rclone sync google-drive: .google-drive-backup/";
+				Unit = {
+					Description = "Backup and sync daemon [SYNC]";
+					Wants = "rclone-sync.timer";
+				};
 			};
-			Service = {
-				Type = "simple";
-				ExecStart = "${pkgs.rclone}/bin/rclone mount google-drive: /home/khushraj/google-drive/ --vfs-cache-mode full";
-				Environment = "PATH=/run/wrappers/bin/:$PATH";
-				Restart = "always";
-				RestartSec = 1;
-				TimeoutStopSec = 10;
+		};
+		timers = {
+			rclone-sync = {
+				Timer.OnCalendar = "*-*-* *:*:00";
+				Install.WantedBy = [ "timers.target" ];
+				Unit = {
+					Description = "Backup and sync timer [SYNC]";
+					Requires = "rclone-sync.service";
+				};
 			};
 		};
 	};
