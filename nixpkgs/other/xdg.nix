a764013d44c0a964c2dd5a5a1968438b65778f9e
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-07-11 23:25:23 +0530

    Add XDG mime type associations

--- nixpkgs/other/xdg.nix
+++ nixpkgs/other/xdg.nix
@@ -4,16 +4,35 @@ let
 	disable = "/home/khushraj"; # Setting directories to the home directory disables them
 in
 {
-	xdg.userDirs = {
-		enable = true;
-		createDirectories = true;
-		desktop     = disable;
-		documents   = disable;
-		download    = "/home/khushraj/Downloads";
-		music       = disable;
-		pictures    = disable;
-		publicShare = disable;
-		templates   = disable;
-		videos      = disable;
+	xdg = {
+		userDirs = {
+			enable = true;
+			createDirectories = true;
+			desktop     = disable;
+			documents   = disable;
+			download    = "/home/khushraj/Downloads";
+			music       = disable;
+			pictures    = disable;
+			publicShare = disable;
+			templates   = disable;
+			videos      = disable;
+		};
+		mimeApps = {
+			enable = true;
+			defaultApplications = {
+				# Links
+				"x-scheme-handler/http" = "chromium-browser.desktop";
+				"x-scheme-handler/https" = "chromium-browser.desktop";
+				"x-scheme-handler/mailspring" = "Mailspring.desktop";
+
+				# Application
+				"application/pdf" = "chromium-browser.desktop";
+
+				# Text
+				"text/html" = "code.desktop";
+				"text/markdown" = "code.desktop";
+				"text/plain" = "code.desktop";
+			};
+		};
 	};
 }
