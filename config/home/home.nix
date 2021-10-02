78f569f3812aa38966702edd6d03d01e1c8415a5
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-10-02 13:08:42 +0530

    Add gnome-system-monitor

--- config/home/home.nix
+++ config/home/home.nix
@@ -62,78 +62,79 @@
 		# Apps
 		(authy.overrideAttrs 
 			(oa: { meta = oa.meta // { priority = 6; }; })
-		)                       # Two-factor authentication app
-		betterdiscordctl        # Discord enhancer
-		bitwarden               # Password manager
-		calibre                 # Ebook library
-		discord                 # Communications app
-		electrum                # Bitcoin wallet
-		element-desktop         # Matrix Chat Client
-		etcher                  # Disk image flasher
-		fritzing                # Hardware design tool
-		gimp                    # Image editor
-		google-chrome           # Browser
-		handbrake               # Audio-video encoder
-		mailspring              # Email client
-		minecraft               # Sandbox game
-		libreoffice             # Office suite
-		stable.mysql-workbench  # Database query tool
-		obsidian                # Document organiser
-		qbittorrent             # Torrent client
-		recapp                  # Screen Recorder
-		signal-desktop          # Communications app
-		slack                   # Communications app
-		spotify                 # Music app
-		tdesktop                # Communications app
-		teams                   # Video call app
-		teamviewer              # Remote desktop app
-		vlc                     # Audio/video player
-		zoom-us                 # Video call app
+		)                          # Two-factor authentication app
+		betterdiscordctl           # Discord enhancer
+		bitwarden                  # Password manager
+		calibre                    # Ebook library
+		discord                    # Communications app
+		electrum                   # Bitcoin wallet
+		element-desktop            # Matrix Chat Client
+		etcher                     # Disk image flasher
+		fritzing                   # Hardware design tool
+		gimp                       # Image editor
+		google-chrome              # Browser
+		handbrake                  # Audio-video encoder
+		mailspring                 # Email client
+		minecraft                  # Sandbox game
+		libreoffice                # Office suite
+		stable.mysql-workbench     # Database query tool
+		obsidian                   # Document organiser
+		qbittorrent                # Torrent client
+		recapp                     # Screen Recorder
+		signal-desktop             # Communications app
+		slack                      # Communications app
+		spotify                    # Music app
+		tdesktop                   # Communications app
+		teams                      # Video call app
+		teamviewer                 # Remote desktop app
+		vlc                        # Audio/video player
+		zoom-us                    # Video call app
 
 		# CLI Tools
-		b3sum                   # BLAKE3 hashing tool 
-		bitwarden-cli           # Password manager CLI
-		bottom                  # Terminal-based task viewer
-		dmg2img                 # Disk image converter
-		du-dust                 # Terminal-based storage space viewer
-		fd                      # File finder
-		ffmpeg                  # Multimedia handler
-		grex                    # Regex maker
-		hyperfine               # Benchmarking tool
-		imagemagick             # Terminal-based photo modifier
-		jq                      # JSON processor
-		neofetch                # Terminal-based system-info viewer
-		ngrok                   # Localhost proxy-tunnel
-		p7zip                   # Compression tool
-		procs                   # Terminal job viewer
-		protonvpn-cli           # Virtual Private Network
-		rclone                  # Cloud sync tool
-		ripgrep                 # File-content searcher
-		sd                      # stdin filterer
-		speedtest-cli           # Network speed measurer
-		tealdeer                # Manual summarizer
-		unzip                   # File unzipper
-		viu                     # Terminal image viewer
-		websocat                # WebSocket client
-		wget                    # File downloader
-		youtube-dl              # Multimedia scraper
+		b3sum                      # BLAKE3 hashing tool 
+		bitwarden-cli              # Password manager CLI
+		bottom                     # Terminal-based task viewer
+		dmg2img                    # Disk image converter
+		du-dust                    # Terminal-based storage space viewer
+		fd                         # File finder
+		ffmpeg                     # Multimedia handler
+		grex                       # Regex maker
+		hyperfine                  # Benchmarking tool
+		imagemagick                # Terminal-based photo modifier
+		jq                         # JSON processor
+		neofetch                   # Terminal-based system-info viewer
+		ngrok                      # Localhost proxy-tunnel
+		p7zip                      # Compression tool
+		procs                      # Terminal job viewer
+		protonvpn-cli              # Virtual Private Network
+		rclone                     # Cloud sync tool
+		ripgrep                    # File-content searcher
+		sd                         # stdin filterer
+		speedtest-cli              # Network speed measurer
+		tealdeer                   # Manual summarizer
+		unzip                      # File unzipper
+		viu                        # Terminal image viewer
+		websocat                   # WebSocket client
+		wget                       # File downloader
+		youtube-dl                 # Multimedia scraper
 
 		# System
-		deja-dup                # Backup tool
-		efibootmgr              # EFI boot entry manager
-		gnome.cheese            # Camera application
-		gnome.gedit             # Text editor
-		gnome.gnome-font-viewer # Font viewer
-		gnome.nautilus          # File system explorer
-		gnome.seahorse          # Keychain viewer
-		gparted                 # Partition manager
-		hsetroot                # Wallpaper setter
-		lm_sensors              # Hardware sensors. Required for i3status-rust
-		pavucontrol             # Audio configuration helper
-		polkit_gnome            # Policy Kit Agent
-		shotwell                # Image viewer
-		v4l-utils               # Video input adjuster
-		yaru-theme              # GTK Theme
+		deja-dup                   # Backup tool
+		efibootmgr                 # EFI boot entry manager
+		gnome.cheese               # Camera application
+		gnome.gedit                # Text editor
+		gnome.gnome-font-viewer    # Font viewer
+		gnome.gnome-system-monitor # Process monitor
+		gnome.nautilus             # File system explorer
+		gnome.seahorse             # Keychain viewer
+		gparted                    # Partition manager
+		hsetroot                   # Wallpaper setter
+		lm_sensors                 # Hardware sensors. Required for i3status-rust
+		pavucontrol                # Audio configuration helper
+		polkit_gnome               # Policy Kit Agent
+		shotwell                   # Image viewer
+		v4l-utils                  # Video input adjuster
+		yaru-theme                 # GTK Theme
 	];
 
 	home.stateVersion = "21.05";
