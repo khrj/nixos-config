34ee554f540b8fbbc552dde2e67d674a3c4a8573
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-12-13 15:56:16 +0530

    Bump flake and rename inputs

--- config/flake.nix
+++ config/flake.nix
@@ -2,21 +2,23 @@
 	description = "NixOS configuration";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 		nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-21.05";
+		nixpkgs-master.url = "github:nixos/nixpkgs";
 		home-manager.url = "github:nix-community/home-manager";
 		cachix.url = "github:jonascarpay/declarative-cachix";
 	};
 
-	outputs = { nixpkgs, nixpkgs-stable, home-manager, cachix, ... }: {
-		nixosConfigurations.khushrajs-desktop = nixpkgs.lib.nixosSystem {
+	outputs = { nixpkgs-unstable, nixpkgs-stable, nixpkgs-master, home-manager, cachix, ... }: {
+		nixosConfigurations.khushrajs-desktop = nixpkgs-unstable.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = 
 			[
 				cachix.nixosModules.declarative-cachix
 				({ pkgs, ... }: {
-					_module.args.nixpkgs-ref = nixpkgs;
+					_module.args.nixpkgs-ref = nixpkgs-unstable;
 					_module.args.stable = import nixpkgs-stable { inherit (pkgs.stdenv.targetPlatform) system; };
+					_module.args.master = import nixpkgs-master { inherit (pkgs.stdenv.targetPlatform) system; };
 					imports = [ ./os/os.nix ];
 				})
 				home-manager.nixosModules.home-manager {
@@ -24,6 +26,7 @@
 					home-manager.useUserPackages = true;
 					home-manager.users.khushraj = { pkgs, ... }: {
 						_module.args.stable = import nixpkgs-stable { inherit (pkgs.stdenv.targetPlatform) system; };
+						_module.args.master = import nixpkgs-master { inherit (pkgs.stdenv.targetPlatform) system; };
 						imports = [ ./home/home.nix ];
 					};
 				}
