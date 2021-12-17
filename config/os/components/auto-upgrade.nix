7668e037c0d66e50bff53d1d27ef6ec521faa778
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-12-18 00:55:26 +0530

    Fix auto-upgrade, update inputs

--- config/os/components/auto-upgrade.nix
+++ config/os/components/auto-upgrade.nix
@@ -4,9 +4,11 @@
 	system.autoUpgrade = {
 		enable = true;
 		dates = "15:00";
+		flake = "/home/khushraj/nixos-config/config";
 		flags = [
-			"--update-input" "nixpkgs"
+			"--update-input" "nixpkgs-unstable"
 			"--update-input" "nixpkgs-stable"
+			"--update-input" "nixpkgs-master"
 			"--update-input" "home-manager"
 			"--update-input" "cachix"
 		];
