cfa1c3832572d7c2233b6bff5c8f8b8b380d36c1
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-01-26 19:48:55 +0530

    Timeout in 1 second if backup drive is not connected at boot; Bump flake

--- config/os/components/hardware.nix
+++ config/os/components/hardware.nix
@@ -4,5 +4,9 @@
 	fileSystems."/mnt/backup" = {
 		device = "/dev/disk/by-uuid/96fb8ee2-7412-4307-9542-5733a0e7c93a";
 		fsType = "btrfs";
+		options = [
+			"nofail"
+			"x-systemd.device-timeout=1"
+		];
 	};
 }
