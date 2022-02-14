cb9f62b40c76da1733362152f5b92d346f2c33c7
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-02-14 20:47:22 +0530

    Replace fileSystems mount on boot with automount

--- config/os/components/hardware.nix
+++ config/os/components/hardware.nix
@@ -1,12 +1,19 @@
 { ... }:
 
 {
-	fileSystems."/mnt/backup" = {
-		device = "/dev/disk/by-uuid/96fb8ee2-7412-4307-9542-5733a0e7c93a";
-		fsType = "btrfs";
-		options = [
-			"nofail"
-			"x-systemd.device-timeout=1ms"
+	systemd = {
+		mounts = [
+			{
+				what = "/dev/disk/by-uuid/96fb8ee2-7412-4307-9542-5733a0e7c93a";
+				where = "/mnt/backup";
+				type = "btrfs";	
+			}
+		];
+		automounts = [
+			{
+				where = "/mnt/backup";
+				wantedBy = [ "multi-user.target" ];
+			}
 		];
 	};
 }
