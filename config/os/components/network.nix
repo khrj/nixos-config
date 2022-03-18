118b07dfb82c7b14896e0b3b1d812847e370a4ef
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-03-18 10:49:41 +0530

    Open TCP 8000

--- config/os/components/network.nix
+++ config/os/components/network.nix
@@ -7,5 +7,6 @@
 		interfaces.enp4s0.useDHCP = true;
 		hostName = "khushrajs-desktop";
 		firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+		firewall.allowedTCPPorts = [ 8000 ];
 	};
 }
