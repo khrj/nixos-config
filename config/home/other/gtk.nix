d2cfdca61f81cce725e94bdf493214477d2f91e5
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-04-07 16:06:35 +0530

    Remove extra CSS required for i3

--- config/home/other/gtk.nix
+++ config/home/other/gtk.nix
@@ -5,14 +5,6 @@
 		enable = true;
 		iconTheme.name = "Yaru";
 		theme.name = "Yaru";
-		gtk3 = {
-			extraConfig.gtk-cursor-theme-name = "Yaru";
-			extraCss = ''
-				.background decoration {
-					padding: 0px;
-					border: 0px;
-				}
-			'';
-		};
+		gtk3.extraConfig.gtk-cursor-theme-name = "Yaru";
 	};
 }
