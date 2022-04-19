d2dbc4a791a99fc9b03e2da1d1897fe240b39d90
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-04-19 12:30:08 +0530

    Don't start MySQL and PostgreSQL on boot

--- config/os/components/programs/mysql.nix
+++ config/os/components/programs/mysql.nix
@@ -1,8 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
 	services.mysql = {
 		enable = true;
 		package = pkgs.mysql80;
 	};
+
+	# Don't start MySQL on boot
+	systemd.services.mysql.wantedBy = lib.mkForce [];
 }
--- config/os/components/programs/postgres.nix
+++ config/os/components/programs/postgres.nix
@@ -1,8 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
     services.postgresql = {
 		enable = true;
 		package = pkgs.postgresql_13;
 	};
+
+	# Don't start PostgreSQL on boot
+	systemd.services.postgresql.wantedBy = lib.mkForce [];
 }
