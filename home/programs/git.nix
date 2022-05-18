0496407a85b00fa76e193db8dc9bf99cdbb559d3
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-05-18 14:47:08 +0530

    Mark all directories as safe (single user system)

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -10,16 +10,18 @@
 			signByDefault = true;
 			key = "19883FD8EA2EB4EC0A931E22B77B2A40E7702F19";
 		};
-		extraConfig = {
-			core = {
-				editor = "code --wait --new-window";
-			};
-			init = {
-				defaultBranch = "main";
-			};
-			tag = {
-				forceSignAnnotated = true;
-			};
-		};
+		extraConfig = ''
+			[core]
+			    editor = "code --wait --new-window"
+
+			[init]
+			    defaultBranch = "main"
+
+			[tag]
+			    forceSignAnnotated = true
+
+			[safe]
+			    directory = *
+		'';
 	};
 }
