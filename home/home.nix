a7be2c91595108eed58a9ab294f94f8cd47310cf
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-10-23 11:53:51 +0530

    Add frp

--- home/home.nix
+++ home/home.nix
@@ -80,6 +80,7 @@
 		du-dust                      # Terminal-based storage space viewer
 		fd                           # File finder
 		ffmpeg                       # Multimedia handler
+		frp                          # Reverse proxy
 		grex                         # Regex maker
 		hyperfine                    # Benchmarking tool
 		imagemagick                  # Terminal-based photo modifier
