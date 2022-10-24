c2f4b3e0585dca044ffefba65caabbe3a3bad690
Khushraj Rathod <khushraj.rathod@gmail.com>
2022-10-24 16:41:43 +0530

    Switch adb to lagging (see nixpkgs/issues/197430)

--- os/components/programs/adb.nix
+++ os/components/programs/adb.nix
@@ -1,6 +1,11 @@
-{ userDetails, ... }:
+{ userDetails, lagging, ... }:
 
 {
-	programs.adb.enable = true;
+	# programs.adb.enable = true;
+
+    services.udev.packages = [ lagging.android-udev-rules ];
+    environment.systemPackages = [ lagging.android-tools ];
+    users.groups.adbusers = {};
+
 	users.users.${userDetails.username}.extraGroups = ["adbusers"];
 }
