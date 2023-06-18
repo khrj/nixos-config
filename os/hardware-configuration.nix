c2e8d93540cf07645b5b91d9f8bdcc1a1dcd0a3f
Khushraj Rathod <khushraj.rathod@gmail.com>
2023-06-18 17:54:58 +0530

    Use tmpfs on root

--- os/hardware-configuration.nix
+++ os/hardware-configuration.nix
@@ -10,9 +10,23 @@
 	boot.kernelModules = [ "kvm-intel" ];
 	boot.extraModulePackages = [ ];
 
+	# Core mounts
+
 	fileSystems."/" = {
+		device = "none";
+		fsType = "tmpfs";
+		options = [ "defaults" "size=2G" "mode=755" ];
+	};
+
+	fileSystems."/boot" = { 
+		device = "/dev/disk/by-uuid/3062-F123";
+		fsType = "vfat";
+	};
+
+	fileSystems."/nix" = {
 		device = "/dev/disk/by-uuid/5bf75184-4297-4ee9-ab62-90fff6ad81ff";
 		fsType = "btrfs";
+		options = [ "subvol=@nix" ];
 	};
 
 	fileSystems."/nix/store" = {
@@ -21,14 +35,22 @@
 		options = [ "bind" ];
 	};
 
-	fileSystems."/boot" = { 
-		device = "/dev/disk/by-uuid/3062-F123";
-		fsType = "vfat";
+	fileSystems."/home/khushraj" = {
+		device = "/dev/disk/by-uuid/5bf75184-4297-4ee9-ab62-90fff6ad81ff";
+		fsType = "btrfs";
+		options = [ "subvol=@home" ];
 	};
 
 	swapDevices = [
 		{ device = "/dev/disk/by-uuid/35b29226-5565-4b3f-9307-4aef991c98f8"; }
 	];
+
+	# Config mounts
+	fileSystems."/etc/machine-id" = { device = "/nix/persist/etc/machine-id"; fsType = "none"; options = [ "bind" ]; };
+	fileSystems."/etc/passwd" = { device = "/nix/persist/etc/passwd"; fsType = "none"; options = [ "bind" ]; };
+	fileSystems."/etc/shadow" = { device = "/nix/persist/etc/shadow"; fsType = "none"; options = [ "bind" ]; };
+	fileSystems."/etc/sudoers" = { device = "/nix/persist/etc/sudoers"; fsType = "none"; options = [ "bind" ]; };
+	fileSystems."/var/log" = { device = "/nix/persist/var/log"; fsType = "none"; options = [ "bind" ]; };
 
 	powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
