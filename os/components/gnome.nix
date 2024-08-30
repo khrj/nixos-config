30396e8deee1aeaf1b41936dde63fa9de9b19233
Khushraj Rathod <khushraj.rathod@gmail.com>
2024-08-31 02:42:33 +0530

    Rename gnome packages

--- os/components/gnome.nix
+++ os/components/gnome.nix
@@ -9,7 +9,7 @@
 
 	environment.systemPackages = with pkgs.gnomeExtensions; [ blur-my-shell ];
 
-	environment.gnome.excludePackages = with pkgs.gnome; [
+	environment.gnome.excludePackages = with pkgs; [
 		# baobab                       # Disk space analyser
 		# cheese                       # Camera application
 		eog
@@ -23,20 +23,20 @@
 		yelp
 		# gnome-calculator             # Calculator
 		gnome-calendar
-		gnome-characters
+		gnome.gnome-characters
 		# gnome-clocks                 # Clock app (stopwatch + timer + alarm)
-		gnome-contacts
+		gnome.gnome-contacts
 		# gnome-font-viewer            # Font viewer
 		# gnome-logs                   # systemd log viewer
-		gnome-maps
-		gnome-music
+		gnome.gnome-maps
+		gnome.gnome-music
 		# gnome-system-monitor         # Process monitor
 		# gnome-text-editor            # Text editor
-		gnome-weather
-		pkgs.gnome-connections
-		pkgs.gnome-console
-		pkgs.gnome-photos
-		pkgs.gnome-tour
-		pkgs.gnome-user-docs
+		gnome.gnome-weather
+		gnome-connections
+		gnome-console
+		gnome-photos
+		gnome-tour
+		gnome-user-docs
 	];
 }
