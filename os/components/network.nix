a07cbd4e3a6450573112bf206c22f85d7553ab8c
Khushraj Rathod <khushraj.rathod@gmail.com>
2024-11-07 14:23:09 +0530

    Enable systemd-resolved to resolve local services and MagicDNS

--- os/components/network.nix
+++ os/components/network.nix
@@ -1,18 +1,21 @@
 { config, ... }:
 
 {
-	environment.etc."resolv.conf.head".text = "nameserver 8.8.8.8";
 	services.openssh.enable = true;
+
+	services.resolved = {
+		enable = true;
+		domains = [ "~." ];
+		fallbackDns = [ "8.8.8.8" "1.1.1.1" ];
+		dnsovertls = "opportunistic";
+	};
+	
 	networking = {
 		useDHCP = false;
 		firewall = {
 			allowedTCPPorts = [ 22 19000 8081 ];
 			allowedUDPPorts = [ config.services.tailscale.port ];
 			checkReversePath = "loose";
-		};
-		hosts = {
-			"10.1.0.10" = [ "campnet.bits-goa.ac.in" ];
-			"103.65.199.163" = [ "quanta.bits-goa.ac.in" ];
 		};
 	};
 }
