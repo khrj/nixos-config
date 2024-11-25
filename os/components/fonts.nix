8a7862eeef9ba86923d1ab1197d657d2daf330f1
Khushraj Rathod <khushraj.rathod@gmail.com>
2024-11-25 17:55:14 +0530

    Change noto-fonts-cjk to -sans

--- os/components/fonts.nix
+++ os/components/fonts.nix
@@ -5,7 +5,7 @@
 		fontconfig.enable = lib.mkForce true;
 		packages = with pkgs; [
 			noto-fonts
-			noto-fonts-cjk
+			noto-fonts-cjk-sans
 			noto-fonts-extra
 			noto-fonts-emoji
 			font-awesome
